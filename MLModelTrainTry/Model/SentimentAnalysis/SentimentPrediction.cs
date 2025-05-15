8df90a52881096997b9bf1279a753993ffdcf3d6
rose3085 <rabinasedhai1@gmail.com>
2025-05-15 18:46:19 +0545

    Reapply "Add project files."
    This reverts commit be759223c16b08b70e5e8f77702b22e175285503.

---
+++ MLModelTrainTry/Model/SentimentAnalysis/SentimentPrediction.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLModelTrainTry.Model.SentimentAnalysis
+{
+    public class SentimentPrediction : SentimentData
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Probability { get; set; }
+
+        public float Score { get; set; }
+    }
+}
